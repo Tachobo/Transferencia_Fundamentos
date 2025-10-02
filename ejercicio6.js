7fd6396bd2bf808ac3c5188118a34f7755075ec3
Tachobo <wilmerardila2001@gmail.com>
2025-10-01 19:49:08 -0500

    segundo commit

--- ejercicio6.js
+++ ejercicio6.js
@@ -1,1 +1,4 @@
-alert("En este progrma se ")
+alert("En este progrma se hara la conversion de minutos a horas");
+let minutos = parseInt(prompt("Por favor digitar los minutos"));
+let horas = minutos/60;
+alert("Las horas son" + " " + horas);
